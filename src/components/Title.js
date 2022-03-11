fa0f396f0e661ab4d1c780f16ad70c18907a3a35
sandeep <sandmulay2008@gmail.com>
2022-03-11 13:03:48 +0530

    commit

--- src/components/Title.js
+++ src/components/Title.js
@@ -4,8 +4,8 @@ export default function Title({name,title}) {
   return (
     <div className='row'>
         <div className='col-10 mx-auto my-2 text-center text-title'>
-            <h1>
-                {name}<strong> {title}</strong>
+            <h1 className='title'>
+            <strong>{name} {title}</strong>
             </h1>
         </div>
 
