ef161e94b2f0eff5b1565f801dfc181266012526
sandeep <sandmulay2008@gmail.com>
2022-04-02 11:28:21 +0530

    commit

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,20 +5,14 @@ font-family:copperplate gothic;
 text-transform:capitalize;
 font-size:1.4rem;
 margin-right:10px;
-border:0.05rem solid white;
+border:0.05rem solid #07130D;
 height:60px;
 background:transparent;
-color:white;
+color:#07130D;
 border-radius:0.5rem;
 padding:0.2rem 0.5rem;
 cursor:pointer;
 transition:all 0.5s ease;
-&:hover{
-  background:#AC3B61 ;
-  color:white;
-}
-&:focous{
-  outline:none;
-}
+
 
 `
